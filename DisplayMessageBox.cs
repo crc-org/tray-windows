8e4b31af52ca25fcb9859bc2fbaea67adb8e21f9
Anjan Nath <kaludios@gmail.com>
2020-04-17 14:41:42 +0530

    Add helpers to display different types of message boxes

---
+++ DisplayMessageBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tray_windows
+{
+    class DisplayMessageBox
+    {
+        public static void Error(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void Warn(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
