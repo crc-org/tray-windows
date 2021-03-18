2c232a0c3ff2dda7cea466eb665e1dc4e4dfd63d
Gerard Braad <me@gbraad.nl>
2021-03-18 08:28:11 +0800

    Use constants for commands
    Signed-off-by: Gerard Braad <me@gbraad.nl>

---
+++ Communication/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CRCTray.Communication
+{
+    static class BasicCommands
+    {
+        internal const string Status = "status";
+        internal const string Version = "version";
+
+        internal const string Start = "start";
+        internal const string Stop = "stop";
+        internal const string Delete = "delete";
+
+        internal const string ConsoleUrl = "webconsoleurl";
+        internal const string Config = "getconfig";
+    }
+
+}
