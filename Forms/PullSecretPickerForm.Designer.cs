93c478be2fa24232e2ed8ac7cec18f0ff8479dc3
Anjan Nath <kaludios@gmail.com>
2021-05-19 01:07:25 +0530

    Add pull secret window ask user to provide a pull secret file if one is not configured

---
+++ Forms/PullSecretPickerForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace CRCTray
+{
+    partial class PullSecretPickerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pullSecretPathTextField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.browseFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please select the pull secret to use";
+            // 
+            // pullSecretPathTextField
+            // 
+            this.pullSecretPathTextField.Location = new System.Drawing.Point(15, 41);
+            this.pullSecretPathTextField.Name = "pullSecretPathTextField";
+            this.pullSecretPathTextField.ReadOnly = true;
+            this.pullSecretPathTextField.Size = new System.Drawing.Size(440, 22);
+            this.pullSecretPathTextField.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pull secret available at";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(162, 70);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(256, 17);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "cloud.redhat.com/openshift/create/local";
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(461, 35);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 35);
+            this.BrowseButton.TabIndex = 4;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(461, 100);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 31);
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // browseFile
+            // 
+            this.browseFile.InitialDirectory = "$env:USERPROFILE";
+            // 
+            // PullSecretPickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 143);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pullSecretPathTextField);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PullSecretPickerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pull Secret Picker";
+            this.Load += new System.EventHandler(this.PullSecretPicker_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pullSecretPathTextField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.OpenFileDialog browseFile;
+    }
+}
---
+++ Forms/PullSecretPickerForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRCTray
+{
+    public partial class PullSecretPickerForm : Form
+    {
+        string pullSecretPath = "";
+        public PullSecretPickerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PullSecretPicker_Load(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(Resource.ocp_logo);
+            Icon = Icon.FromHandle(bm.GetHicon());
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            browseFile.ShowDialog();
+            if (browseFile.FileName != "")
+            { 
+                pullSecretPath = browseFile.FileName;
+                pullSecretPathTextField.Text = pullSecretPath;
+            }
+
+            Console.WriteLine("Selected file: {0}", pullSecretPath);
+        }
+
+        public String ShowFilePicker()
+        {
+            base.ShowDialog();
+            return pullSecretPath;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
