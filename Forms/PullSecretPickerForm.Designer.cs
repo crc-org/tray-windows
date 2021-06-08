04fffdb63510156d43635306aef9ce4be111facb
Anjan Nath <kaludios@gmail.com>
2021-06-08 12:14:12 +0530

    Issue #76 Align controls in pull secret picker form

--- Forms/PullSecretPickerForm.Designer.cs
+++ Forms/PullSecretPickerForm.Designer.cs
@@ -36,13 +36,15 @@ namespace CRCTray
             this.BrowseButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
             this.browseFile = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(236, 18);
             this.label1.TabIndex = 0;
@@ -50,7 +52,7 @@ namespace CRCTray
             // 
             // pullSecretPathTextField
             // 
-            this.pullSecretPathTextField.Location = new System.Drawing.Point(15, 41);
+            this.pullSecretPathTextField.Location = new System.Drawing.Point(3, 35);
             this.pullSecretPathTextField.Name = "pullSecretPathTextField";
             this.pullSecretPathTextField.ReadOnly = true;
             this.pullSecretPathTextField.Size = new System.Drawing.Size(440, 22);
@@ -59,7 +61,7 @@ namespace CRCTray
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 70);
+            this.label2.Location = new System.Drawing.Point(15, 103);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(150, 17);
             this.label2.TabIndex = 2;
@@ -68,7 +70,7 @@ namespace CRCTray
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(162, 70);
+            this.linkLabel1.Location = new System.Drawing.Point(160, 103);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(256, 17);
             this.linkLabel1.TabIndex = 3;
@@ -77,9 +79,9 @@ namespace CRCTray
             // 
             // BrowseButton
             // 
-            this.BrowseButton.Location = new System.Drawing.Point(461, 35);
+            this.BrowseButton.Location = new System.Drawing.Point(450, 35);
             this.BrowseButton.Name = "BrowseButton";
-            this.BrowseButton.Size = new System.Drawing.Size(75, 35);
+            this.BrowseButton.Size = new System.Drawing.Size(73, 26);
             this.BrowseButton.TabIndex = 4;
             this.BrowseButton.Text = "Browse";
             this.BrowseButton.UseVisualStyleBackColor = true;
@@ -87,9 +89,9 @@ namespace CRCTray
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(461, 100);
+            this.okButton.Location = new System.Drawing.Point(462, 103);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 31);
+            this.okButton.Size = new System.Drawing.Size(73, 25);
             this.okButton.TabIndex = 5;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
@@ -99,17 +101,31 @@ namespace CRCTray
             // 
             this.browseFile.InitialDirectory = "$env:USERPROFILE";
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pullSecretPathTextField, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BrowseButton, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(526, 64);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
             // PullSecretPickerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(550, 143);
             this.Controls.Add(this.okButton);
-            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.pullSecretPathTextField);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -117,6 +133,8 @@ namespace CRCTray
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pull Secret Picker";
             this.Load += new System.EventHandler(this.PullSecretPicker_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,5 +149,6 @@ namespace CRCTray
         private System.Windows.Forms.Button BrowseButton;
         private System.Windows.Forms.Button okButton;
         private System.Windows.Forms.OpenFileDialog browseFile;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
