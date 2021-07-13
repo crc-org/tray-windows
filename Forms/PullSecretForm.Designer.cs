8cf44a8d262dd8452da04e9878b56e450c5455f2
Gerard Braad <me@gbraad.nl>
2021-07-13 17:05:05 +0800

    Fixes #99 Adds copy-paste dialog for pull-secret

---
+++ Forms/PullSecretForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace CRCTray
+{
+    partial class PullSecretForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.browseFile = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pullSecreTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(370, 305);
+            this.okButton.Margin = new System.Windows.Forms.Padding(2);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(58, 20);
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Please paste here the pull secret to start the cluster.\r\n It will be saved in the" +
+    " secure store.";
+            // 
+            // pullSecreTextbox
+            // 
+            this.pullSecreTextbox.Location = new System.Drawing.Point(15, 43);
+            this.pullSecreTextbox.Multiline = true;
+            this.pullSecreTextbox.Name = "pullSecreTextbox";
+            this.pullSecreTextbox.Size = new System.Drawing.Size(413, 257);
+            this.pullSecreTextbox.TabIndex = 7;
+            // 
+            // PullSecretForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(440, 336);
+            this.Controls.Add(this.pullSecreTextbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.okButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PullSecretForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pull Secret Picker";
+            this.Load += new System.EventHandler(this.PullSecretPicker_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.OpenFileDialog browseFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pullSecreTextbox;
+    }
+}
---
+++ Forms/PullSecretForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRCTray.Helpers;
+
+namespace CRCTray
+{
+    public partial class PullSecretForm : Form
+    {
+        
+        public PullSecretForm()
+        {
+            InitializeComponent();
+
+            PullSecret = String.Empty;
+        }
+
+        private void PullSecretPicker_Load(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(Resource.ocp_logo);
+            Icon = Icon.FromHandle(bm.GetHicon());
+        }
+
+        public String PullSecret
+        {
+            get;
+            set;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            PullSecret = pullSecreTextbox.Text;
+            Hide();
+        }
+    }
+}
