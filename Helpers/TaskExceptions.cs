5ac6159ef3bda47209d57e53357ce540af0500d2
Anjan Nath <kaludios@gmail.com>
2021-09-06 14:24:41 +0530

    Add TryTaskWithRetry helper method to retry tasks that are canceled or times out

---
+++ Helpers/TaskExceptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CRCTray.Helpers
+{
+    internal class RetryableTaskFailedException : Exception
+    {
+        public RetryableTaskFailedException(string message) : base(message) { }
+    }
+}
